efc07dc8a1c4af1fb17bb8ac204bfc8185551c7d
mukta07072006 <worldamity994@gmail.com>
2026-08-16 20:32:33 +0600

    lgin

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.BETTER_AUTH_URL ?? process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000',
+    baseURL: process.env.BETTER_AUTH_URL ?? 'https://property-rental-client-jet.vercel.app',
 })
 
 export const { signIn, signUp, useSession } = createAuthClient()
